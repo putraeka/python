56495b1a86eb21817b46b662365495cc2d0aead7
putraeka <gh@eka.gdn>
2019-06-27 23:46:15 +0700

    akhirnya bisa scrape harga juga, susah bener

--- dev-ed/scrape-amazon.py
+++ dev-ed/scrape-amazon.py
@@ -1,16 +1,13 @@
 import requests
-
 from bs4 import BeautifulSoup
 
 URL = 'https://www.amazon.de/Sony-Digitalkamera-Touch-Display-Vollformatsensor-Kartenslots/dp/B07B4L1PQ8'
-
 headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0'}
-
 page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.content, 'lxml')
 
-title = soup.find(id="productTitle").get_text()
+# title = soup.find(id="productTitle").get_text()
 #price = soup.find('div', attrs={id="olp-sl-new-used", "class_":"a-color-price"}).get_text()
 #pricegroup = soup.find(id="olp-sl-new-used")
 #price = soup.find_all(id="productTitle", {'class': 'a-color-price'}) 
@@ -19,5 +16,14 @@ title = soup.find(id="productTitle").get_text()
 #print(title.strip())
 #print(price)
 
-price = soup.find('olp-padding-right')
-print(price)
+# price = soup.find_all('span')
+# price_tag = price.find_all('olp-sl-new-used')
+# for tag in price_tag:
+#     print(tag)
+# for tag in price:
+#     if "olp-sl-new-used" in price.attrs:
+#         print(tag)
+
+price = soup.find_all(class_='a-color-price')[2].string
+price_stripped = price[0:5]
+print(price_stripped)
