92f04da90cbc7d59850d6b2bc6996ce4534dbcc5
putraeka <gh@eka.gdn>
2019-07-05 14:00:31 +0700

    belajar module re

---
+++ computational-linguistics/belajar-re.py
@@ -0,0 +1,12 @@
+import re
+
+f = open('mobydick.txt', 'r')
+lines = f.readlines()
+
+for line in lines:
+    # jika menggunakan re.match kita hanya menemukan kata yang berada
+    # di awal line
+    # tapi jika menggunakan re.search, jika akan menemukan kata didalam setiap line
+    if re.search("Ahab", line):
+        line = line.strip()
+        print(line)
