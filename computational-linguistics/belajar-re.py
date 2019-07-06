686e01ed03bdc2dd4238a300424aefafe2d85fd5
putraeka <gh@eka.gdn>
2019-07-06 10:57:53 +0700

    belajar modul regex

--- computational-linguistics/belajar-re.py
+++ computational-linguistics/belajar-re.py
@@ -6,7 +6,10 @@ lines = f.readlines()
 for line in lines:
     # jika menggunakan re.match kita hanya menemukan kata yang berada
     # di awal line
-    # tapi jika menggunakan re.search, jika akan menemukan kata didalam setiap line
+    # tapi jika menggunakan re.search, jika akan menemukan 
+    # kata didalam setiap line
     if re.search("Ahab", line):
         line = line.strip()
         print(line)
+        
+f.close()
