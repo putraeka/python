667ef973624906c0a33a7a9e5bb4e999ded24c28
putraeka <gh@eka.gdn>
2019-07-11 18:41:01 +0700

    storing scraped data to sqlite3

--- buildwithpython/quotetutorial/quotetutorial/pipelines.py
+++ buildwithpython/quotetutorial/quotetutorial/pipelines.py
@@ -5,9 +5,37 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
 
 class QuotetutorialPipeline(object):
-    def process_item(self, item, spider):
+    
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
         
-        print("Pipeline :" + item['title'][0])
+    def create_connection(self):
+        self.conn = sqlite3.connect("myquotes.db")
+        self.curr = self.conn.cursor()
+        
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+        self.curr.execute("""create table quotes_tb(
+            title text,
+            author text,
+            tag text
+            )""")
+        
+    def process_item(self, item, spider):
+        self.store_db(item)
+        
+        # print("Pipeline :" + item['title'][0])
         return item
+    
+    def store_db(self, item):
+        self.curr.execute("""insert into quotes_tb values (?,?,?)""",(
+            item['title'][0],
+            item['author'][0],
+            item['tag'][0]
+        ))
+        
+        self.conn.commit()
