f8c5ac558e7c38fa88bcc31b212277c06ad70fb8
putraeka <gh@eka.gdn>
2019-07-12 22:25:35 +0700

    exercise dictionary

---
+++ arkadiusz-wlodarczyk/27-exercise-dictionary.py
@@ -0,0 +1,38 @@
+"""
+write a program that will allow the user to:
+    1) Add new definitions
+    2) Search existing definitions
+    3) Delete the definitions that he has chosen
+"""
+dictionary = {}
+
+while (True):
+    print("1: Add definition")
+    print("2: Search for the  definition")
+    print("3: Remove definition")
+    print("4: End")
+
+    choice = input("What do you want to do? ")
+
+    if (choice == "1"):
+        key = input("Enter the word (key) to define: ")
+        definition = input("Enter definition: ")
+        dictionary[key] = definition
+        print("Definition added succesfully")
+    elif (choice == "2"):
+        key = input("What are you searching for? ")
+        if key in dictionary:
+            print(dictionary[key])
+        else:
+            print("Definition has not been found: ", key)
+    elif (choice == "3"):
+        key = input("What definition do you want to remove? ")
+        print("Definition", key, "removed succesfully")
+        if key in dictionary:
+            del dictionary[key]    
+        else:
+            print("Definition has not been found: ", key)
+
+    elif (choice == "4"):
+        print("Bye, then!")
+        break
